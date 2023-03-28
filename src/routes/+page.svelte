5b8d17a2f25688d874f60159c692aeba8fcb2616
Luis <luisfernandezvazquez@gmail.com>
2023-03-28 23:19:22 +0200

    Added more breakpoints + Instagram button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,47 +5,126 @@
     </div>
   </div>
   <div class="background">
-    <img class="wave" src="/img/wave_p.svg" alt="wave" />
+    <div class="wave" />
   </div>
+  <a href="https://www.instagram.com/lamagatcafe/">
+    <div class="instagram" />
+  </a>
 </div>
 
 <style>
-  .page {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  @media (max-width: 992px) and (orientation: portrait) {
+    .page {
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    .header {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background-color: white;
+      padding: 20px 0 10px 0;
+    }
+
+    .logo {
+      width: 300px;
+    }
+
+    .logo img {
+      width: 100%;
+    }
+
+    .wave {
+      background-image: url(/img/wave_p.svg);
+      background-repeat: no-repeat;
+      background-size: 100%;
+      height: 50px;
+      filter: drop-shadow(10px 10px 5px black);
+    }
+
+    .background {
+      background-image: url(/img/bbw.jpg);
+      background-repeat: no-repeat;
+      background-position: right -225px top 0;
+      background-color: black;
+      background-size: 1400px;
+      width: 100%;
+      flex: 1;
+    }
   }
 
-  .header {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: white;
-    padding: 20px 0 10px 0;
+  @media (min-width: 992px), (orientation: landscape) {
+    .page {
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+
+    .header {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      background-color: white;
+      padding: 0px 10px 0 20px;
+    }
+
+    .logo {
+      width: 250px;
+    }
+
+    .logo img {
+      width: 100%;
+    }
+
+    .wave {
+      background-image: url(/img/wave_l.svg);
+      background-repeat: no-repeat;
+      background-size: auto 100%;
+      height: 100vh;
+      width: 25px;
+      filter: drop-shadow(10px 10px 5px black);
+    }
+
+    .background {
+      background-image: url(/img/bbw.jpg);
+      background-repeat: no-repeat;
+      background-position: right -225px top 0;
+      background-size: 1400px;
+      background-color: black;
+      width: 100%;
+      flex: 1;
+    }
   }
 
-  .logo {
-    width: 300px;
+  @media (min-width: 1200px) {
+    .background {
+      background-position: top 0 left 0;
+      background-size: cover;
+    }
   }
 
-  .logo img {
-    width: 100%;
-  }
-
-  .wave {
-    width: 100%;
-    filter: drop-shadow(10px 10px 5px black);
-  }
-
-  .background {
-    background-image: url(/img/bbw.jpg);
+  .instagram {
+    width: 76px;
+    height: 76px;
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    background-image: url(/img/instagram.svg);
     background-repeat: no-repeat;
-    background-position: right -225px top 0;
-    background-color: black;
-    background-size: 1400px;
-    width: 100%;
-    flex: 1;
+    background-size: 50px 50px;
+    background-position: 13px 13px;
+    background-color: white;
+    border-radius: 20px 20px;
+    filter: drop-shadow(0px 10px 5px black);
+  }
+
+  .instagram:hover {
+    filter: invert(1);
   }
 </style>
