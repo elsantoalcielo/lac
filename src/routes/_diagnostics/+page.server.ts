3305b3b8eb56a96d58620dcc195195b50006ac78
Luis <luisfernandezvazquez@gmail.com>
2023-03-29 23:22:27 +0200

    Removed diagnostics

--- src/routes/_diagnostics/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { env } from '$env/dynamic/private'
-
-export const load = (async ({ params }) => {
-  return JSON.parse(JSON.stringify(env))
-}) satisfies PageServerLoad;
