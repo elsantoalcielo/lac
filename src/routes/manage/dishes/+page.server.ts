463f61689b7277de929497769dccb62d39902df0
Luis <luisfernandezvazquez@gmail.com>
2023-05-30 23:04:40 +0200

    Starting dishes management

---
+++ src/routes/manage/dishes/+page.server.ts
@@ -0,0 +1,30 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { env } from '$env/dynamic/private'
+
+import { MongoClient } from 'mongodb';
+import { translateAndCapitalize } from '$lib/utils';
+
+const AUTHORIZED_USERS = env.AUTHORIZED_USERS.split(',');
+
+export const load: PageServerLoad = async (event) => {
+
+  const session = await event.locals.getSession();
+  if (AUTHORIZED_USERS.indexOf(session?.user?.email as string) == -1) {
+    throw redirect(303, '/auth/signin?csrf=true');
+  }
+
+  let dishes;
+
+  const client = new MongoClient(env.MONGODB_URI);
+  try {
+    const database = client.db('amagat');
+    const collection = database.collection('dishes');
+    dishes = await collection.find().toArray();
+    console.log("LFV" + JSON.stringify(dishes))
+  } finally {
+    await client.close();
+  }
+
+  return { dishes: JSON.parse(JSON.stringify(dishes)) }
+}
---
+++ src/routes/manage/dishes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  export let data: PageData;
+  import { signOut } from "@auth/sveltekit/client";
+
+  import Allergens from "$lib/components/allergens.svelte";
+  import Translations from "$lib/components/translations.svelte";
+  import type { Dish, Menu } from "$lib/types";
+
+  let dishes: Dish[] = data.dishes as Dish[];
+</script>
+
+<div class="list">
+  {#each dishes as dish}
+    <div class="dish">
+      {dish.name}
+    </div>
+  {/each}
+</div>
+
+<style>
+
+  .list {
+    margin: 10px;
+  }
+
+  .dish {
+    margin-bottom: 10px;
+  }
+
+</style>
