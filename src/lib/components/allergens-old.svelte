09b106310211cd7df5354d77c3d2a7ed22335e38
Luis <luisfernandezvazquez@gmail.com>
2023-06-20 14:43:05 +0200

    Restore file deleted by mistake

---
+++ src/lib/components/allergens-old.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  const allergensList = [
+    "gluten",
+    "crustaceans",
+    "eggs",
+    "fish",
+    "peanuts",
+    "soybeans",
+    "milk",
+    "nuts",
+    "celery",
+    "mustard",
+    "sesame",
+    "sulphites",
+    "lupin",
+    "molluscs",
+  ];
+
+  export let values: string[] = [];
+
+  let open = false;
+
+  const changed = (e: Event & { currentTarget: EventTarget & HTMLInputElement }, allergen: string) => {
+    const index = values.indexOf(allergen);
+    if ((e.target as HTMLInputElement).checked) {
+      if (index == -1) {
+        values.push(allergen);
+      }
+    } else {
+      if (index > -1) {
+        values.splice(index, 1);
+      }
+    }
+  };
+</script>
+
+<div>
+  <button on:click={() => (open = !open)}>!</button>
+  {#if open}
+    <div>
+      {#each allergensList as allergen, index}
+        <div>
+          <label>
+            <input type="checkbox" checked={values.indexOf(allergen) > -1} on:input={(e) => changed(e, allergen)} />{allergen}
+          </label>
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
+
+<style>
+  button {
+    border: solid 1px black;
+    border-radius: 5px;
+  }
+
+  img {
+    width: 20px;
+  }
+</style>
