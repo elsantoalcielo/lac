bee185c6107bc39b2d1154b51473876e1423dbb1
Luis <luisfernandezvazquez@gmail.com>
2023-06-28 18:16:53 +0200

    Use a language list

--- src/lib/components/language.svelte
+++ src/lib/components/language.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
+  import { LANGUAGES } from "$lib/languages";
 
   export let language: string;
 
   const onChange = (event: any) => {
     location.replace(`${location.pathname}?l=${event.target.value}`);
-  }
-
+  };
 </script>
 
 <select bind:value={language} on:change={onChange}>
-  <option value="ca">català</option>
-  <option value="es">castellano</option>
-  <option value="en">english</option>
-  <option value="fi">suomi</option>
-  <option value="ko">한국어</option>
+  {#each LANGUAGES as language}
+    <option value={language.locale}>{language.name}</option>
+  {/each}
 </select>
---
+++ src/lib/languages.ts
@@ -0,0 +1,28 @@
+export type Language = {
+  locale: string,
+  name: string
+}
+
+export const LANGUAGES: Language[] =
+  [
+    {
+      locale: "ca",
+      name: "català",
+    },
+    {
+      locale: "es",
+      name: "castellano",
+    },
+    {
+      locale: "en",
+      name: "english",
+    },
+    {
+      locale: "fi",
+      name: "suomi",
+    },
+    {
+      locale: "ko",
+      name: "한국어",
+    }
+  ];
