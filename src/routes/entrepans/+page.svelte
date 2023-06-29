867c60ac3e61746fb9121bdddc60a9546e34250f
Luis <luisfernandezvazquez@gmail.com>
2023-06-30 00:14:39 +0200

    More sandwiches development

--- src/routes/entrepans/+page.svelte
+++ src/routes/entrepans/+page.svelte
@@ -10,230 +10,84 @@
 <svelte:head>
   <title>L'Amagat Cafè - {title}</title>
 </svelte:head>
-<div class="menu">
-  <div class="logo" on:click={() => location.href = '/'}>
-    <img src="/img/logo.png" alt="Logo" />
+
+<div class="m-auto max-w-[1024px] flex flex-col items-center font-['Montserrat'] font-bold">
+  <div class="mt-2.5 cursor-pointer" on:click={() => (location.href = "/")}>
+    <img class="w-52" src="/img/logo.png" alt="Logo" />
   </div>
 
   {#if sandwichMenu.title}
-    <div class="title">
+    <div>
       {sandwichMenu.title}
     </div>
   {/if}
 
   {#each sandwichMenu.sections as section}
-    <div class="section">
+    <div class="flex flex-col items-center mb-3 w-full">
       {#if section.title}
-        <div class="title">{section.title}</div>
+        <div class="mb-1 text-2xl border-dotted border-black border-b-[1px] text-center">{section.title}</div>
       {/if}
-      {#each section.sandwiches as sandwich}
-        <div class="dish">
-          <div class="name">
-            {sandwich.name}
-          </div>
-          <div class="allergens">
-            {#if sandwich.allergens}
-              {#each sandwich.allergens as allergen}
-                <img src="/img/allergens/{allergen}.jpg" alt={allergen} />
-              {/each}
-            {/if}
-          </div>
-          <div class="price">{sandwich.price} €</div>
-          <!--
-          {#if dish.description}
-            <div class="description">{dish.description}</div>
-          {/if}
-          -->
+      <div class="w-[90%]">
+        <div class="flex mt-1.5">
+          <div class="basis-1/2" />
+          <div class="basis-1/4 text-right">MITJÀ</div>
+          <div class="basis-1/4 text-right">GRAN</div>
         </div>
-      {/each}
-      <div class="filigrana" />
+        {#each section.sandwiches as sandwich}
+          <div class="flex mt-1.5">
+            <div class="flex basis-1/2 items-center">
+              <div class="text-lg">
+                {sandwich.name}
+              </div>
+              <div class="flex items-center">
+                {#if sandwich.allergens}
+                  {#each sandwich.allergens as allergen}
+                    <img class="w-5 h-5 ml-1.5" src="/img/allergens/{allergen}.jpg" alt={allergen} />
+                  {/each}
+                {/if}
+              </div>
+            </div>
+            <div class="basis-1/4 text-xl flex items-center justify-end">
+              {#if sandwich.price.medium}
+                {sandwich.price.medium} €
+              {/if}
+            </div>
+            <div class="flex flex-wrap basis-1/4 items-center justify-end text-xl">
+              {#if sandwich.large_variant}
+                <div class="text-sm md:mr-2">{sandwich.large_variant}</div>
+              {/if}
+              <div>{sandwich.price.large} €</div>
+            </div>
+          </div>
+        {/each}
+      </div>
     </div>
   {/each}
 
-  <div class="allergen-list">
-    <div>
-      <img src="/img/allergens/gluten.jpg" alt="gluten" /><span>{translateAllergen("gluten", language)}</span>
+  <div class="w-[100px] h-[25px] bg-center bg-contain bg-no-repeat" style="background-image: url(/img/filigrana.png)" />
+
+  <div class="flex justify-center font-['Lora'] font-bold mt-4">
+    <div class="flex">
+      <img class="w-5 h-5 ml-1.5" src="/img/allergens/gluten.jpg" alt="gluten" /><span class="ml-1.5 mr-3">{translateAllergen("gluten", language)}</span>
     </div>
-    <div><img src="/img/allergens/eggs.jpg" alt="eggs" /><span>{translateAllergen("eggs", language)}</span></div>
-    <div><img src="/img/allergens/fish.jpg" alt="fish" /><span>{translateAllergen("fish", language)}</span></div>
-    <div>
-      <img src="/img/allergens/milk.jpg" alt="milk" /><span>{translateAllergen("milk", language)}</span>
+    <div class="flex">
+      <img class="w-5 h-5 ml-1.5" src="/img/allergens/eggs.jpg" alt="eggs" /><span class="ml-1.5 mr-3">{translateAllergen("eggs", language)}</span>
+    </div>
+    <div class="flex">
+      <img class="w-5 h-5 ml-1.5" src="/img/allergens/fish.jpg" alt="fish" /><span class="ml-1.5 mr-3">{translateAllergen("fish", language)}</span>
+    </div>
+    <div class="flex">
+      <img class="w-5 h-5 ml-1.5" src="/img/allergens/milk.jpg" alt="milk" /><span class="ml-1.5 mr-3">{translateAllergen("milk", language)}</span>
     </div>
   </div>
 
-  <div class="footer">{footer}</div>
+  <div class="mt-5 pt-2.5 border-dotted border-t-[1px] border-t-black max-w-[90%] text-center text-xl">{footer}</div>
 
-  <div class="language">
+  <div class="absolute right-1 top-1">
     <Language language={data.language} />
   </div>
 </div>
 
 <style>
-  @import url(../../css/reset.css);
   @import url(../../../static/fonts/fonts.css);
-
-  .language {
-    position: absolute;
-    right: 5px;
-    top: 5px;
-  }
-
-  .logo {
-    margin-top: 10px;
-    cursor: pointer;
-  }
-
-  .logo img {
-    width: 200px;
-  }
-
-  .menu {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    background-color: white;
-    color: black;
-    font-family: Montserrat;
-    font-size: 20px;
-    font-weight: bold;
-  }
-
-  .menu > .title {
-    font-size: xx-large;
-  }
-
-  .section {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 20px;
-  }
-
-  .section > .title {
-    font-size: x-large;
-  }
-
-  .dish {
-    margin: 10px 0 0 0;
-  }
-
-  @media (max-width: 768px) {
-    .dish {
-      display: grid;
-      grid-template-columns: auto auto;
-      width: 90%;
-      border-bottom: dotted 1px gray;
-      padding-bottom: 10px;
-    }
-
-    .dish:nth-last-child(2) {
-      border-bottom: none;
-    }
-
-    .dish .name {
-      grid-column: 1 / 3;
-    }
-
-    .dish .price {
-      text-align: right;
-    }
-
-    .allergen-list {
-      display: flex;
-      max-width: 90%;
-      flex-wrap: wrap;
-      justify-content: center;
-    }
-  }
-
-  @media (min-width: 768px) {
-    .dish {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-    }
-
-    .dish .price {
-      margin-left: 10px;
-    }
-
-    .allergen-list {
-      display: flex;
-      max-width: 80%;
-      flex-wrap: wrap;
-      justify-content: center;
-    }
-  }
-
-  .dish .name {
-    font-size: large;
-    text-align: left;
-  }
-
-  .dish .description {
-    font-style: italic;
-  }
-
-  .allergens {
-    display: flex;
-  }
-
-  .allergens img,
-  .allergen-list img {
-    width: 20px;
-    margin-left: 5px;
-    vertical-align: text-top;
-  }
-
-  .allergen-list {
-    font-family: Lora;
-    font-size: smaller;
-  }
-
-  .allergen-list div {
-    display: flex;
-  }
-
-  .allergen-list span {
-    margin-left: 5px;
-    margin-right: 10px;
-  }
-
-  .footer {
-    margin-top: 20px;
-    padding-top: 10px;
-    border-top: dotted 1px black;
-    text-align: center;
-  }
-
-  .filigrana {
-    background-image: url(/img/filigrana.png);
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: contain;
-    width: 100px;
-    height: 25px;
-    margin-top: 10px;
-  }
-
-  /* printing */
-
-  @page {
-    size: A4 landscape;
-  }
-
-  @media print {
-    .language {
-      display: none;
-    }
-
-    .footer {
-      font-size: smaller;
-    }
-
-    .filigrana {
-      -webkit-print-color-adjust: exact !important;
-    }
-  }
 </style>
