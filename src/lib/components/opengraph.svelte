aaaf5e63b22b53f73b6a2a3ba1776980dd1d10ad
Luis <luisfernandezvazquez@gmail.com>
2023-07-09 22:31:07 +0200

    Trying to get rid of Meta OpenGraph warnings

--- src/lib/components/opengraph.svelte
+++ src/lib/components/opengraph.svelte
@@ -3,10 +3,11 @@
   export let path: string;
 </script>
 
-<meta name="og:title" content={title} />
-<meta name="og:url" content="https://www.lamagatcafe.com/{path}" />
-<meta name="og:type" content="website" />
-<meta name="og:description" content="Lloc petit i bufó al cor del Guinardó. Proposta senzilla però feta amb amor. Cafè, entrepans, plats, i bona atenció" />
-<meta name="og:image" content="https://www.lamagatcafe.com/img/og.png" />
-<meta name="og:image:width" content="1200" />
-<meta name="og:image:height" content="600" />
+<meta property="og:title" content="{title}" />
+<meta property="og:url" content="https://www.lamagatcafe.com/{path}" />
+<meta property="og:type" content="website" />
+<meta property="og:description" content="Lloc petit i bufó al cor del Guinardó. Proposta senzilla però feta amb amor. Cafè, entrepans, plats, i bona atenció" />
+<meta property="og:site_name" content="L'Amagat Cafè" />
+<meta property="og:image" content="https://www.lamagatcafe.com/img/og.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="600" />
